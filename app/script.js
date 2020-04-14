01373bdffc791ccbd5d612fa50df65f8bf78f51d
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-04-14 22:27:23 +0200

    finish app

--- app/script.js
+++ app/script.js
@@ -20,7 +20,7 @@ class App extends React.Component {
     this.setState({ time: this.state.time - 1 });
 
     if(this.state.time === 0) {
-        this.setState ({
+      this.setState ({
         status: this.state.status === 'work'? 'rest' : 'work',
         time: this.state.status === 'work'? 20 : 1200,
       });
@@ -43,11 +43,11 @@ class App extends React.Component {
       status: 'off',
       time: 0,
       timer: null,
-    })
+    });
   }
 
   componentWillUnmount () {
-    clearInterval(this.timerInterval)
+    clearInterval(this.timerInterval);
   }
 
   closeApp() {
@@ -63,7 +63,7 @@ class App extends React.Component {
     return (
       <div>
         <h1>Protect your eyes</h1>
-        {this.state.status == 'off' ? (
+        {this.state.status === 'off' ? (
           <section>
             <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
             <p>This app will help you track your time and inform you when it's time to rest.</p>
@@ -89,8 +89,8 @@ class App extends React.Component {
         ):''}
         <button className="btn btn-close" onClick={this.closeApp}>X</button>
       </div>
-    )
+    );
   }
-};
+}
 
 render(<App />, document.querySelector('#app'));

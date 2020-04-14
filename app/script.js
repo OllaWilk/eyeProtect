6727b05842b36d5a036097fc5d8b9ac8a8ebca25
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-04-14 13:40:46 +0200

    add state and conditioning to show items

--- app/script.js
+++ app/script.js
@@ -3,14 +3,32 @@ import { render } from 'react-dom';
 
 class App extends React.Component {
 
+  state = {
+    status: 'off',
+    time: '00:01',
+    timer: null,
+  }
+
   render() {
     return (
       <div>
         <h1>Protect your eyes</h1>
+        {this.state.status == 'off'? (
+          <section>
         <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
         <p>This app will help you track your time and inform you when it's time to rest.</p>
-        <img src="./images/work.png" />
-        <img src="./images/rest.png" />
+          </section>
+        ): ''}
+
+        {this.state.status === 'work'? (
+          <img src="./images/work.png" />
+        ):''}
+
+        {this.state.status === 'rest'? (
+          <img src="./images/work.png" />
+        ):''}
+
+
         <div className="timer">
           18:23
         </div>

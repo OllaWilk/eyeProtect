d62390eda12bdb15856a246d1c0088681eba83f8
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-04-14 21:18:48 +0200

    add the counter format and active start

--- app/script.js
+++ app/script.js
@@ -13,10 +13,21 @@ class App extends React.Component {
     const minutes = Math.floor(allSeconds/60);
     const seconds = Math.floor(allSeconds - (minutes*60));
 
-    //return `${(minutes < 10 ? `0${minutes}` : minutes)} : ${(seconds < 10 ? `0${seconds}` : seconds)}`;
+    return `${(minutes < 10 ? `0${minutes}` : minutes)} : ${(seconds < 10 ? `0${seconds}` : seconds)}`;
+  }
 
-    const timeOutput = `${(minutes < 10 ? `0${minutes}` : minutes)} : ${(seconds < 10 ? `0${seconds}` : seconds)}`;
-    return timeOutput;
+  step() {
+    console.log('ON')
+  };
+
+  startTimer = () => {
+    this.setState({
+      status: 'work',
+      time: 1200,
+      timer: setInterval(() =>
+      this.step(), 1000
+      )
+    });
   }
 
   render() {
@@ -27,22 +38,22 @@ class App extends React.Component {
           <section>
             <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
             <p>This app will help you track your time and inform you when it's time to rest.</p>
-            <button className="btn">Start</button>
+            <button className="btn" onClick={this.startTimer}>Start</button>
           </section>
         ):''}
 
         {this.state.status == 'work' ? (
-          <img src="./images/work.png" />
+          <img src="./images/Work.png" />
         ): ''}
 
         {this.state.status == 'rest' ? (
-          <img src="./images/rest.png" />
+          <img src="./images/Rest.png" />
         ):''}
 
         {this.state.status !== 'off' ? (
           <section>
             <div className="timer">
-              18:23
+              {this.formatTime(this.state.time)}
             </div>
             <button className="btn">Stop</button>
           </section>
